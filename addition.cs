ce1e90af961fd624ad78a7d5d4bc9cf280b9c66b
Nishahu <111382661+Nishahu@users.noreply.github.com>
2022-08-16 23:17:50 +0530

    Add files via upload

---
+++ addition.cs
@@ -0,0 +1,10 @@
+class addition{
+static void Main(){
+int a=0,n=20; 
+for(a=0;a<=n;a++)
+if(a%2==0)
+System.Console.WriteLine(a);
+else
+System.Console.WriteLine("odd");
+}
+}
---
+++ loop.cs
@@ -0,0 +1,16 @@
+class loop{
+static void Main(){
+ int i;
+i=1;
+while(i<2){
+int j=0;
+while(j<5){
+System.Console.Write("a" "b");
+j=j+1;
+}
+
+System.Console.WriteLine();
+i=i+1;
+}
+}
+}
